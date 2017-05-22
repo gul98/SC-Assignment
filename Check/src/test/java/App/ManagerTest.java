ffb27909eb8e29ccd28cde16f07250d518b8d461
gul98 <gulzainali@gmail.com>
2017-05-22 21:07:43 +0500

    Add files via upload

--- Check/src/test/java/App/ManagerTest.java
+++ Check/src/test/java/App/ManagerTest.java
@@ -5,9 +5,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -17,36 +15,23 @@ import static org.junit.Assert.*;
  * Created by gulza on 22/05/2017.
  */
 @RunWith(Arquillian.class)
-public class AppTest {
-    @Before
-    public void setUp() throws Exception {
-    }
-
-    @After
-    public void tearDown() throws Exception {
-    }
-
+public class ManagerTest {
     @Test
-    public void register() throws Exception {
+    public void deleteEmployee() throws Exception {
+
+        Manager manager= new Manager();
+        manager.addEmployee("test1","pass");
         App app= new App();
-        Assert.assertEquals(app.authenticate("test","pass"),false);
-        app.Register("test","password",0);
-        Assert.assertEquals(app.authenticate("test","pass"),true);
-    }
+        Assert.assertEquals(app.authenticate("test1","pass"),true);
+        manager.deleteEmployee("test1");
+        Assert.assertEquals(app.authenticate("test1","pass"),false);
 
-
-
-    @Test
-    public void getType() throws Exception {
-        App app= new App();
-        Integer actual=0;
-        Assert.assertEquals(app.getType("test"),actual);
     }
 
     @Deployment
     public static JavaArchive createDeployment() {
         return ShrinkWrap.create(JavaArchive.class)
-                .addClass(App.class)
+                .addClass(Manager.class)
                 .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
